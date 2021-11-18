3a559f674c938d60dc260a5525ff412978cc0fb9
Chris Murphy <theworldisyours@gmail.com>
2021-11-18 18:59:04 -0500

    updated push

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int secretNumber = 42;
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Guess the secret number!");
+                Console.Write($"Guess number {i}: ");
+                var input = int.Parse(Console.ReadLine());
+
+                if (secretNumber == input)
+                {
+                    Console.WriteLine("CORRECT!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                };
+            }
         }
     }
 }
