7561137b319d62f886aa052e74a3efb743cc384d
Chris Murphy <theworldisyours@gmail.com>
2021-11-19 14:54:53 -0500

    final commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GuessingGame
 {
@@ -6,12 +7,18 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            int secretNumber = 42;
 
-            for (int i = 1; i < 5; i++)
+            Console.WriteLine("Choose Difficulty: 1 -easy, 2 -medium, 3 -hard, 4 -hardest, 5 -cheater");
+            var difficulty = int.Parse(Console.ReadLine());
+            int numberOfGuesses = new int[] { 8, 6, 4, 2, 1 }[difficulty - 1];
+
+            int secretNumber = new Random().Next(1, 101);
+
+            while (difficulty == 5 || numberOfGuesses > 0)
             {
+                string guessesLeft = difficulty == 5 ? "INFINITE!" : numberOfGuesses.ToString();
                 Console.WriteLine("Guess the secret number!");
-                Console.Write($"Guess number {i}: ");
+                Console.Write($"Your Guess (Guesses left: {guessesLeft}): ");
                 var input = int.Parse(Console.ReadLine());
 
                 if (secretNumber == input)
@@ -19,11 +26,15 @@ namespace GuessingGame
                     Console.WriteLine("CORRECT!");
                     break;
                 }
+                else if (secretNumber > input)
+                {
+                    Console.WriteLine("Your guess is too low!");
+                }
                 else
                 {
-                    Console.WriteLine("WRONG!");
-                };
-            }
+                    Console.WriteLine("Your guess is too high!");
+                }
+            };
         }
     }
 }
